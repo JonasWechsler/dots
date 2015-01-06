a59461bd0ed16b4de3f843ccc33d9fa03dd19ffc
jonaswechsler <jonaswechsler@gmail.com>
2015-01-06 11:58:53 -0600

    setup stuff

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
-sudo apt-get install -y beep xscreensaver conky 
+sudo apt-get install -y beep xscreensaver conky feh terminator
+sudo apt-get install i3
 
 sudo modprobe pcspkr
 
 wget http://github.com/downloads/djmelik/archey/archey-0.2.8.deb
 sudo dpkg -i archey-0.2.8.deb
 
+cp -ir i3 ~/.i3/
+cp -i Pictures/* ~/Pictures/
+cp -ir terminator/* ~/.config/terminator/
+cp -i vimrc ~/.vimrc
