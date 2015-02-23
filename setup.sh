40b774ac2e77ac9f772ed6ae46b113b3d2d8e737
JonasWechsler <jonaswechsler@gmail.com>
2015-02-23 07:40:43 -0600

    Xresources and xinitrc

--- setup.sh
+++ setup.sh
@@ -6,7 +6,9 @@ sudo modprobe pcspkr
 wget http://github.com/downloads/djmelik/archey/archey-0.2.8.deb
 sudo dpkg -i archey-0.2.8.deb
 
-cp -ir i3 ~/.i3/
+cp -ir i3/* ~/.i3/
 cp -i Pictures/* ~/Pictures/
 cp -ir terminator/* ~/.config/terminator/
 cp -i vimrc ~/.vimrc
+cp -ir xinitrc ~/.xinitrc
+cp -ir Xresources ~/.Xresources
