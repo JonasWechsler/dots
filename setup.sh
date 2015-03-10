2511b1f1290627b21b207c7b56b6eb6496d81a50
JonasWechsler <jonaswechsler@gmail.com>
2015-03-10 08:25:22 -0500

    i3-patched patched

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
-sudo apt-get install -y beep xscreensaver conky feh terminator
-sudo apt-get install i3
+#sudo apt-get install -y beep xscreensaver conky feh terminator
+#sudo apt-get install i3
+#
+#sudo modprobe pcspkr
+#
+#wget http://github.com/downloads/djmelik/archey/archey-0.2.8.deb
+#sudo dpkg -i archey-0.2.8.deb
 
-sudo modprobe pcspkr
-
-wget http://github.com/downloads/djmelik/archey/archey-0.2.8.deb
-sudo dpkg -i archey-0.2.8.deb
-
-cp -ir i3/* ~/.i3/
-cp -i Pictures/* ~/Pictures/
-cp -ir terminator/* ~/.config/terminator/
-cp -i vimrc ~/.vimrc
-cp -ir xinitrc ~/.xinitrc
-cp -ir Xresources ~/.Xresources
-cp -ir conkyrc ~/.conkyrc
+#cp -ir i3/* ~/.i3/
+#cp -i Pictures/* ~/Pictures/
+#cp -ir terminator/* ~/.config/terminator/
+#cp -i vimrc ~/.vimrc
+#cp -ir xinitrc ~/.xinitrc
+#cp -ir Xresources ~/.Xresources
+#cp -ir conkyrc ~/.conkyrc
 
 git clone https://github.com/Airblader/i3.git i3-patched
 cd i3-patched
